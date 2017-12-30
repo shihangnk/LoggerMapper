5e7ed6148279e16b1642de0fb20ad6fd03fc6a28
shihangnk <shi_hang_nk@hotmail.com>
2017-12-30 16:43:43 -0600

    init

---
+++ LoggerMapper/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace LoggerMapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogFile = new System.Windows.Forms.TextBox();
+            this.textBoxMapperFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnMap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogFile
+            // 
+            this.textBoxLogFile.Location = new System.Drawing.Point(148, 15);
+            this.textBoxLogFile.Name = "textBoxLogFile";
+            this.textBoxLogFile.Size = new System.Drawing.Size(422, 25);
+            this.textBoxLogFile.TabIndex = 0;
+            // 
+            // textBoxMapperFile
+            // 
+            this.textBoxMapperFile.Location = new System.Drawing.Point(148, 90);
+            this.textBoxMapperFile.Name = "textBoxMapperFile";
+            this.textBoxMapperFile.Size = new System.Drawing.Size(422, 25);
+            this.textBoxMapperFile.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Log file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mapper file";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 162);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(948, 427);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnMap
+            // 
+            this.btnMap.Location = new System.Drawing.Point(706, 18);
+            this.btnMap.Name = "btnMap";
+            this.btnMap.Size = new System.Drawing.Size(277, 90);
+            this.btnMap.TabIndex = 5;
+            this.btnMap.Text = "Mapping";
+            this.btnMap.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 619);
+            this.Controls.Add(this.btnMap);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMapperFile);
+            this.Controls.Add(this.textBoxLogFile);
+            this.Name = "Form1";
+            this.Text = "LoggerMapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogFile;
+        private System.Windows.Forms.TextBox textBoxMapperFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnMap;
+    }
+}
+
